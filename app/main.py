0af893bca20cd3a7f832293f92b1856eba4813e1
VanekForest <zazc256@gmail.com>
2025-05-23 13:04:33 +0300

    feature: добавил логирование 422 ошибки

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
+import logging
 from contextlib import asynccontextmanager
 
+from fastapi.exceptions import RequestValidationError
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
+
 from api import router
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
 from lawly_db.db_models.db_session import global_init
 
 from config import settings
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
@@ -16,6 +25,18 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="Lawly User API", lifespan=lifespan)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+    # or logger.error(f'{exc}')
+    logger.error(request, exc_str)
+    content = {'status_code': 10422, 'message': exc_str, 'data': None}
+    return JSONResponse(
+        content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
+    )
+
 
 app.add_middleware(
     CORSMiddleware,
